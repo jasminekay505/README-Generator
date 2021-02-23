a72f30749b148b3887d1e9edbfc57068363029d3
Jasmine Kay <jasminekay505@gmail.com>
2021-02-22 18:03:45 -0800

    Added packages needed for app and set up array of questions

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
+const fs  = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instruction?',
+        name: 'installation'
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
